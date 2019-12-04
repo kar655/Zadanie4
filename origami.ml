946228079484add19ebb90e7039a48db15d6cf13
kar655 <karol123zag@gmail.com>
2019-12-04 23:12:21 +0100

    final

--- origami.ml
+++ origami.ml
@@ -1,8 +1,11 @@
 (** Punkt na płaszczyźnie *)
 type point = float * float
 
-type kartka = point -> int
 (** Poskładana kartka: ile razy kartkę przebije szpilka wbita w danym punkcie *)
+type kartka = point -> int
+
+(* Kwadrat liczby *)
+let square x = x *. x
 
 (** [prostokat p1 p2] zwraca kartkę, reprezentującą domknięty prostokąt
 o bokach równoległych do osi układu współrzędnych i lewym dolnym rogu [p1]
@@ -19,11 +22,18 @@ let prostokat ((x1, y1): point) ((x2, y2): point) =
 (** [kolko p r] zwraca kartkę, reprezentującą kółko domknięte o środku w punkcie [p] i promieniu [r] *)
 let kolko ((x1, y1): point) r =
   (fun (x2, y2) ->
-    if sqrt((x2 -. x1) *. (x2 -. x1) +. (y2 -. y1) *. (y2 -. y1)) <= r then 1
+    if sqrt (square (x2 -. x1) +. square (y2 -. y1)) <= r then 1
     else 0
   : kartka)
 
-let square x = x *. x
+
+(* odbija punkt wzgledem prostej *)
+let gen a b c x y =
+  ((square b -. square a) *. x -. 2.0 *. a *. b *. y -. 2.0 *. a *. c) /. (square a +. square b)
+
+(* wartosc funckji w punkcie *)
+let value x y a b c =
+  a *. x +. b *. y +. c
 
 (** [zloz p1 p2 k] składa kartkę [k] wzdłuż prostej przechodzącej przez
 punkty [p1] i [p2] (muszą to być różne punkty). Papier jest składany
@@ -38,27 +48,55 @@ let zloz ((x1, y1): point) ((x2, y2): point) (kar: kartka) =
   let a = y1 -. y2 in
   let b = x2 -. x1 in
   let c = x1 *. y2 -. y1 *. x2 in
-  let value = fun x y -> y *. b +. x *. a +. c in
-
-  let gen a b c x y =
-    ((square b -. square a) *. x -. 2.0 *. a *. b *. y -. 2.0 *. a *. c) /. (square a +. square b)
-  in
 
   (fun (x3, y3) ->
-    let v = value x3 y3 in
+    let v = value x3 y3 a b c in
     if v > 0.0 then kar (x3, y3) + kar (gen a b c x3 y3, gen b a c y3 x3)
     else if v = 0.0 then kar (x3, y3)
     else 0
   : kartka)
 
-
-
 (** [skladaj [(p1_1,p2_1);...;(p1_n,p2_n)] k = zloz p1_n p2_n (zloz ... (zloz p1_1 p2_1 k)...)]
 czyli wynikiem jest złożenie kartki [k] kolejno wzdłuż wszystkich prostych
 z listy *)
 let skladaj (lista: (point * point) list) (kar: kartka) =
-  let rec helper lis k =
-    match lista with
-    | (p1, p2)::t -> helper t (zloz p1 p2 k)
-    | [] -> k
-  in helper lista kar
+  List.fold_left (fun a (p1, p2) -> zloz p1 p2 a ) kar lista
+
+
+
+
+(* Przykladowe testy *)
+(*
+let op=[((0.0,1.0),(9.0,7.0));((10.0,10.0),(9.0,3.0));((9.0,10.0),(10.0,7.0));((1.0,2.0),(1.0,7.0));((10.0,3.0),(2.0,9.0))];;
+let kartka=prostokat (0.,0.) (10.,10.) ;;
+let test0=skladaj op kartka;;
+assert (test0 (6.0,0.0)=0);;
+let op=[((7.0,6.0),(7.0,10.0));((10.0,10.0),(5.0,3.0));((2.0,7.0),(10.0,10.0));((7.0,4.0),(6.0,2.0));((10.0,3.0),(5.0,5.0))];;
+let kartka=prostokat (0.,0.) (10.,10.) ;;
+let test1=skladaj op kartka;;
+assert (test1 (9.0,8.0)=0);;
+let op=[((9.0,6.0),(3.0,1.0));((5.0,7.0),(9.0,2.0));((2.0,5.0),(8.0,3.0));((2.0,10.0),(4.0,5.0));((0.0,4.0),(6.0,9.0))];;
+let kartka=kolko (5.,5.) 4. ;;
+let test2=skladaj op kartka;;
+assert (test2 (2.0,6.0)=0);;
+let op=[((7.0,4.0),(6.0,6.0));((8.0,4.0),(2.0,7.0));((4.0,0.0),(4.0,8.0));((3.0,0.0),(0.0,9.0));((3.0,1.0),(0.0,6.0))];;
+let kartka=kolko (5.,5.) 4. ;;
+let test3=skladaj op kartka;;
+assert (test3 (1.0,8.0)=0);;
+let op=[((0.0,2.0),(2.0,10.0));((7.0,8.0),(3.0,8.0));((1.0,3.0),(10.0,7.0));((8.0,7.0),(10.0,8.0));((1.0,6.0),(4.0,3.0))];;
+let kartka=kolko (5.,5.) 4. ;;
+let test4=skladaj op kartka;;
+assert (test4 (6.0,9.0)=0);;
+let op=[((9.0,5.0),(10.0,10.0));((10.0,8.0),(0.0,2.0));((2.0,10.0),(0.0,6.0));((5.0,0.0),(0.0,9.0));((10.0,10.0),(7.0,3.0))];;
+let kartka=prostokat (0.,0.) (10.,10.) ;;
+let test5=skladaj op kartka;;
+assert (test5 (8.0,0.0)=0);;
+let op=[((0.0,8.0),(3.0,0.0));((2.0,3.0),(9.0,5.0));((0.0,7.0),(5.0,1.0));((6.0,8.0),(8.0,3.0));((0.0,9.0),(2.0,5.0))];;
+let kartka=prostokat (0.,0.) (10.,10.) ;;
+let test6=skladaj op kartka;;
+assert (test6 (0.0,2.0)=0);;
+let op=[((4.0,7.0),(7.0,7.0));((6.0,10.0),(3.0,9.0));((2.0,2.0),(3.0,5.0));((1.0,3.0),(2.0,8.0));((7.0,0.0),(10.0,2.0))];;
+let kartka=kolko (5.,5.) 4. ;;
+let test7=skladaj op kartka;;
+assert (test7 (10.0,8.0)=0);;
+*)
